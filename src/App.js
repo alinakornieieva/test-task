ab2ab7c1c258cdb912c9fea6d3da16d54ee27249
alinakornieieva <alinakorneeva401@gmail.com>
2023-08-19 12:35:27 +0300

    Created SelectType, added redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
+import { SelectType } from './components/SelectType/SelectType.js'
 
 const App = () => {
   return <BrowserRouter>
     <Navbar />
+    <Routes>
+      <Route path="/" element={<Navigate to="/create" />} />
+      <Route path='/create' Component={SelectType} />
+    </Routes>
   </BrowserRouter>
 }
 
---
+++ src/components/SelectType/SelectType.js
@@ -0,0 +1,8 @@
+import './SelectType.scss'
+
+export const SelectType = () => {
+    return <div className='select'>
+        <button className="btn btn-outline-primary">Create order request</button>
+        <button className="btn btn-outline-primary">Create delivery request</button>
+    </div>
+}
